61193a1cc434aa41fed20b1a24269b88b590c6f5
Marina Rachlin <Marina.rachlin@gmail.com>
2024-01-23 22:19:37 +0200

    font changed

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -12,7 +12,9 @@ export default function Document() {
         />
         <link rel="icon" href="/assets/img/sm-logo.svg" />
         <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css"></link>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-xk1XoiRD6fnFuLftdIJ6tbjLh/+q0w2q3CZ5hFtC5xuCO/5bIItuKPommeIC8DW2g6zgYG7z4yv8LtaBz6lAag==" crossOrigin="anonymous" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link href="https://fonts.googleapis.com/css2?family=Shrikhand&display=swap" rel="stylesheet"/>
+
         
 
       </Head>
