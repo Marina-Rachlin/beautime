6fba5a45ef574f4ee4e953b534af24592a740ef5
Marina Rachlin <Marina.rachlin@gmail.com>
2024-02-07 16:07:39 +0200

    message

--- src/components/home/FeatureProduct.jsx
+++ src/components/home/FeatureProduct.jsx
@@ -10,137 +10,137 @@ const FeatureProduct = ({ props }) => {
 
    const pageSize = 8;
    const page = 1;
-  //  const[products, setProducts] = useState([]);
+   const[products, setProducts] = useState([]);
   const {isLoading,data, error} = useGetAllProductsShopQuery({page, pageSize});
 
-  // useEffect(() => {
-  //   if(isLoading){
-  //     return;
-  //   }
-  //   else if(data){
-  //     console.log('here :',data)
-  //     const productsData = data.products.map(((item) => ({
-  //       _id: item._id,
-  //       name: item.name,
-  //       category: item.category,
-  //       brand: item.brand,
-  //       price: item.price,
-  //       discountPrice: item.discountPrice,
-  //       images: item.images,
-  //       stock: item.stock,
-  //       ratings: item.ratings,
-  //       commentsCount: item.commentsCount,
-  //       purchased: item.purchased,
-  //       isNew: item.isNew,
-  //       isHot: item.isHot,
-  //       discount: item.discount
-  //     })))
-  //     setProducts(productsData);
-  //   }
-  //   else{
-  //     console.log(error);
-  //   }
-  // }, [data])
+  useEffect(() => {
+    if(isLoading){
+      return;
+    }
+    else if(data){
+      console.log('here :',data)
+      const productsData = data.products.map(((item) => ({
+        _id: item._id,
+        name: item.name,
+        category: item.category,
+        brand: item.brand,
+        price: item.price,
+        discountPrice: item.discountPrice,
+        images: item.images,
+        stock: item.stock,
+        ratings: item.ratings,
+        commentsCount: item.commentsCount,
+        purchased: item.purchased,
+        isNew: item.isNew,
+        isHot: item.isHot,
+        discount: item.discount
+      })))
+      setProducts(productsData);
+    }
+    else{
+      console.log(error);
+    }
+  }, [data])
   
 
-const products = [
-  {
-    name: 'Crystal-Infused Highlighter',
-    slug: 'crystal-infused-highlighter',
-    image: '/assets/img/products/product-img-1.png',
-    category: 'Makeup',
-    price: 150.00,
-    originalPrice: 200.00,
-    sizes: ['250 ml', '500 ml', '1000 ml', '1500 ml'],
-    isHot: true,
-    discount: 15,
-    isNew: false,
-  },
-  {
-    name: 'Makeup Smooth Brushes',
-    slug: 'makeup-smooth-brushes',
-    image: '/assets/img/products/product-img-2.png',
-    category: 'Makeup',
-    price: 132.00,
-    originalPrice: 200.00,
-    sizes: [],
-    isHot: false,
-    discount: 0,
-    isNew: true,
-  },
-  {
-    name: 'Eyeshadow Palette',
-    slug: 'eyeshadow-palette',
-    image: '/assets/img/products/product-img-3.png',
-    category: 'Makeup',
-    price: 25.00,
-    originalPrice: 30.00,
-    sizes: [],
-    isHot: false,
-    discount: 0,
-    isNew: true,
-  },
-  {
-    name: 'Moisturizing Cream',
-    slug: 'moisturizing-cream',
-    image: '/assets/img/products/product-img-4.png',
-    category: 'Skincare',
-    price: 45.00,
-    originalPrice: 50.00,
-    sizes: ['50 ml', '100 ml'],
-    isHot: false,
-    discount: 10,
-    isNew: false,
-  },
-  {
-    name: 'Hair Conditioner',
-    slug: 'hair-conditioner',
-    image: '/assets/img/products/product-img-5.png',
-    category: 'Hair Care',
-    price: 12.00,
-    originalPrice: 15.00,
-    sizes: ['250 ml'],
-    isHot: true,
-    discount: 20,
-    isNew: false,
-  },
-  {
-    name: 'Moisturizing Cream',
-    slug: 'moisturizing-cream',
-    image: '/assets/img/products/product-img-4.png',
-    category: 'Skincare',
-    price: 45.00,
-    originalPrice: 50.00,
-    sizes: ['50 ml', '100 ml'],
-    isHot: false,
-    discount: 10,
-    isNew: false,
-  },
-  {
-    name: 'Hair Conditioner',
-    slug: 'hair-conditioner',
-    image: '/assets/img/products/product-img-5.png',
-    category: 'Hair Care',
-    price: 12.00,
-    originalPrice: 15.00,
-    sizes: ['250 ml'],
-    isHot: true,
-    discount: 20,
-    isNew: false,
-  },
-  {
-    name: 'Eyeshadow Palette',
-    slug: 'eyeshadow-palette',
-    image: '/assets/img/products/product-img-3.png',
-    category: 'Makeup',
-    price: 25.00,
-    originalPrice: 30.00,
-    sizes: [],
-    isHot: false,
-    discount: 0,
-    isNew: true,
-  },
-];
+// const products = [
+//   {
+//     name: 'Crystal-Infused Highlighter',
+//     slug: 'crystal-infused-highlighter',
+//     image: '/assets/img/products/product-img-1.png',
+//     category: 'Makeup',
+//     price: 150.00,
+//     originalPrice: 200.00,
+//     sizes: ['250 ml', '500 ml', '1000 ml', '1500 ml'],
+//     isHot: true,
+//     discount: 15,
+//     isNew: false,
+//   },
+//   {
+//     name: 'Makeup Smooth Brushes',
+//     slug: 'makeup-smooth-brushes',
+//     image: '/assets/img/products/product-img-2.png',
+//     category: 'Makeup',
+//     price: 132.00,
+//     originalPrice: 200.00,
+//     sizes: [],
+//     isHot: false,
+//     discount: 0,
+//     isNew: true,
+//   },
+//   {
+//     name: 'Eyeshadow Palette',
+//     slug: 'eyeshadow-palette',
+//     image: '/assets/img/products/product-img-3.png',
+//     category: 'Makeup',
+//     price: 25.00,
+//     originalPrice: 30.00,
+//     sizes: [],
+//     isHot: false,
+//     discount: 0,
+//     isNew: true,
+//   },
+//   {
+//     name: 'Moisturizing Cream',
+//     slug: 'moisturizing-cream',
+//     image: '/assets/img/products/product-img-4.png',
+//     category: 'Skincare',
+//     price: 45.00,
+//     originalPrice: 50.00,
+//     sizes: ['50 ml', '100 ml'],
+//     isHot: false,
+//     discount: 10,
+//     isNew: false,
+//   },
+//   {
+//     name: 'Hair Conditioner',
+//     slug: 'hair-conditioner',
+//     image: '/assets/img/products/product-img-5.png',
+//     category: 'Hair Care',
+//     price: 12.00,
+//     originalPrice: 15.00,
+//     sizes: ['250 ml'],
+//     isHot: true,
+//     discount: 20,
+//     isNew: false,
+//   },
+//   {
+//     name: 'Moisturizing Cream',
+//     slug: 'moisturizing-cream',
+//     image: '/assets/img/products/product-img-4.png',
+//     category: 'Skincare',
+//     price: 45.00,
+//     originalPrice: 50.00,
+//     sizes: ['50 ml', '100 ml'],
+//     isHot: false,
+//     discount: 10,
+//     isNew: false,
+//   },
+//   {
+//     name: 'Hair Conditioner',
+//     slug: 'hair-conditioner',
+//     image: '/assets/img/products/product-img-5.png',
+//     category: 'Hair Care',
+//     price: 12.00,
+//     originalPrice: 15.00,
+//     sizes: ['250 ml'],
+//     isHot: true,
+//     discount: 20,
+//     isNew: false,
+//   },
+//   {
+//     name: 'Eyeshadow Palette',
+//     slug: 'eyeshadow-palette',
+//     image: '/assets/img/products/product-img-3.png',
+//     category: 'Makeup',
+//     price: 25.00,
+//     originalPrice: 30.00,
+//     sizes: [],
+//     isHot: false,
+//     discount: 0,
+//     isNew: true,
+//   },
+// ];
 
   return (
     <div className="feature-product-section mb-110">
