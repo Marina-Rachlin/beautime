7ff5b09163666ab790f45c1ea4a20c7f2fc73a7d
Marina Rachlin <Marina.rachlin@gmail.com>
2024-02-07 16:05:06 +0200

    message

--- src/components/common/ProductCard.jsx
+++ src/components/common/ProductCard.jsx
@@ -62,7 +62,7 @@ const ProductCard = ({ product }) => {
     debouncedUpdateCart(productId); // Update server
   };
 
-  
+
   return (
     <div className="col-xl-3 col-lg-4 col-sm-6 col-6">
       <div className="product-card2">
